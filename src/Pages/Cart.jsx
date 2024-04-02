71f737b9e162f90d487317759bcc5ed016831d6e
gungunsaluja <gungunsaluja16@gmail.com>
2024-04-03 01:42:49 +0530

    commit

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -11,6 +11,15 @@ const Cart = () =>{
                         return <CartItem key = {item.id} item = {item} itemIndex = {index}/>
                     }) }
                         </div>
+                        <div>
+                            <div>Your Cart</div>
+                            <div>Summary</div>
+                            <p>
+                                <span>Total Items:{cart.length}
+
+                                </span>
+                            </p>
+                            </div>
                     </div>
             ):(
                 <div>
