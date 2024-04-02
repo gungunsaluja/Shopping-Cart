61ae2b63749e793682c8c0f448544d6428c83d5b
gungunsaluja <gungunsaluja16@gmail.com>
2024-04-03 01:26:07 +0530

    commit

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,10 +1,26 @@
 import { useSelector } from "react-redux";
+import CartItem from "../Components/CartItem";
 
 const Cart = () =>{
     const {cart} = useSelector((state)=>state);
     return (<div>
         {
-            cart.length > 0 ? ():()
+            cart.length > 0 ?(
+                <div>
+                    <div>{cart.map((item,index)=>{
+                        return <CartItem key = {item.id} item = {item} itemIndex = {index}/>
+                    }) }
+                        </div>
+                    </div>
+            ):(
+                <div>
+                    <h1>Cart Empty</h1>
+                    <Link to = {"/"}>
+                    <button>Shop Now</button>
+
+                    </Link>
+                    </div>
+            )
         }
     </div>
 
